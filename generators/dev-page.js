d23c4592c96deba697195cd784660059b16a0027
Russ Carver <russcarverg@gmail.com>
2018-08-13 12:48:15 -0700

    Lint updates.

--- generators/dev-page.js
+++ generators/dev-page.js
@@ -1,7 +1,7 @@
 const createFile = require('../scripts/create-file');
 
 const path = './src/app/dev-landing-page/get-page.ts';
-let finalOutput = `import * as devLandingPage from 'app/dev-landing-page';
+let finalOutput = `import * as devLandingPage from 'app/dev-landing-page'; // tslint:disable-line:no-unused-import
 
 export const getPage: Function = (): string => {
   // devLandingPage.presetProfile();
