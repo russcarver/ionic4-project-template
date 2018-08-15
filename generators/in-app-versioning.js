05e9151ad60a50e9e75cabb2a1fc4f74d452fc22
Russ Carver <russcarverg@gmail.com>
2018-08-14 19:09:15 -0700

    Version logging.

--- generators/in-app-versioning.js
+++ generators/in-app-versioning.js
@@ -18,7 +18,7 @@ fs.readFile('config.xml', 'utf8', function (err, data) {
 
     // Get app version
     appVersion = result['widget']['$']['version'];
-    console.log(`App version for TypeScript being set to: ${appVersion}`);
+    console.log('App version for TypeScript being set to: ' + appVersion);
     
     const versionCode = `export const appVersion: string = '${appVersion}'; // DO NOT Change! This is generated in the in-app-versioning.js script during pre-build (from config.xml)\n`;
     createFile('./src/app-version.ts', versionCode);
