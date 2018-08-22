16e6bab5f0c7c2def39aaab78a0450dafb715b4d
Russ Carver <russcarverg@gmail.com>
2018-08-22 16:27:39 -0700

    Leave space for iOS status bar (updates).

--- src/shared/components/status-bar/status-bar.ts
+++ src/shared/components/status-bar/status-bar.ts
@@ -4,6 +4,4 @@ import { Component } from '@angular/core';
   selector: 'cmp-status-bar',
   template: ''
 })
-export class StatusBarComponent {
-  public static readonly height: number = 20; // If updated, make sure to update css variable `--status-bar__height` in status-bar.scss
-}
+export class StatusBarComponent {}
