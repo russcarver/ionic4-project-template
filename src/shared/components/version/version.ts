a719adcbc1ce1b6edbe17525ffbc9f8985584f8d
Russ Carver <russcarverg@gmail.com>
2018-10-09 12:58:26 -0700

    Versioning update.

--- src/shared/components/version/version.ts
+++ src/shared/components/version/version.ts
@@ -9,14 +9,21 @@ import { Platform } from '@ionic/angular';
   templateUrl: 'version.html'
 })
 export class VersionComponent implements AfterViewInit {
-  public appVer: string = '0.0.0';
+  public appVer: string;
+
+  private readonly defaultAppVer: string = '0.0.0';
 
   public constructor(private appVersion: AppVersion, private platform: Platform) {}
 
   public async ngAfterViewInit(): Promise<void> {
+    this.appVer = this.defaultAppVer;
+
     await this.platform.ready();
     if (this.platform.is('ios') || this.platform.is('android')) {
       this.appVer = await this.appVersion.getVersionNumber();
+      if (this.appVer === '0') { // Happens occsionally on web builds
+        this.appVer = this.defaultAppVer;
+      }
     }
   }
 }
