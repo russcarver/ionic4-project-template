84a1589604ae8a3bf5945f4b781875da117be680
Russ Carver <russcarverg@gmail.com>
2019-07-23 22:00:56 -0600

    Updates for the Jest framework.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,4 +1,2 @@
-// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import 'core-js/es7/reflect';
-// This is used to polyfill on IOS - used for detecting when an image comes into view
 import 'zone.js/dist/zone'; // Included with Angular CLI.
+import './zone-flags.ts';
--- src/setupJest.ts
+++ src/setupJest.ts
@@ -1,5 +1,27 @@
-// tslint:disable
-
 import 'jest-preset-angular';
 import 'jest-zone-patch';
-import './jestGlobalMocks';
+
+const mock: any = (): any => {
+  let storage: any = {};
+  return {
+    clear: (): any => storage = {},
+    getItem: (key: any): any => key in storage ? storage[key] : null,
+    removeItem: (key: any): boolean => delete storage[key],
+    setItem: (key: any, value: any): string => storage[key] = value || ''
+  };
+};
+
+Object.defineProperty(window, 'localStorage', { value: mock() });
+Object.defineProperty(window, 'sessionStorage', { value: mock() });
+Object.defineProperty(window, 'getComputedStyle', {
+  value: (): string[] => ['-webkit-appearance']
+});
+
+Object.defineProperty(document.body.style, 'transform', {
+  value: (): Object => {
+    return {
+      configurable: true,
+      enumerable: true
+    };
+  }
+});
